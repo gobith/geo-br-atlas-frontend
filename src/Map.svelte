81fdb8c8ae79fea0b1702c1f76d4080b2165985b
Georges <gkgobith@gmail.com>
2021-09-28 17:59:07 +0200

    borders

---
+++ src/Map.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  export let map;
+
+  const drawBorders = (borders) => {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.setLineDash([10, 6]);
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "gray";
+    ctx.fillStyle = "gray";
+
+    borders.forEach((border) => {
+        ctx.stroke(border.path)
+    })
+  };
+
+  onMount(() => {
+    console.log(map);
+    drawBorders(map.borders);
+  });
+</script>
+
+<canvas id="canvas" width="10000" height="10000" />
+
+<style>
+</style>
---
+++ src/map-store.ts
@@ -0,0 +1,24 @@
+import { writable } from "svelte/store";
+
+const map = writable(null);
+
+fetch("/map")
+  .then((response) => response.json())
+  .then((mapData) => {
+    map.set(mapFor(mapData));
+  });
+
+export default map;
+
+const mapFor = (mapData) => {
+  const borders = mapData.borders.map((border) => {
+    let path = new Path2D(border.d);
+    return { ...border, path };
+  });
+
+  const areas = mapData.areas.map((area) => {
+    let path = new Path2D(area.d);
+    return {...area , path};
+  });
+  return {borders , areas}
+};
